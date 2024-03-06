bb7b808d59cf63b97646fdac18fac8512e3c5bfc
DESKTOP-RLIRLHO\user <love01040701320@gmail.com>
2024-03-06 15:44:09 +0900

    반응형 작업

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@ window.addEventListener("load", function () {
   // AOS적용
   AOS.init();
 
-  // 언어 펼침 기능
+  // ============== 모달창 ==============
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal-wrap");
+  modal.addEventListener("click", function () {
+    // modal.style.display = "none";
+    // fadeOut(modal);
+    anime({
+      targets: ".modal",
+      delay: 100,
+      duration: 500,
+      opacity: 0,
+      easing: "easeInOutQuad",
+      complete: function () {
+        modal.style.display = "none";
+        body.classList.add("active");
+      },
+    });
+  });
+
+  // ====== 언어 펼침 기능 =====
   const langWord = document.querySelector(".language-word");
   const language = document.querySelector(".languge");
   const languageLi = document.querySelector(".languge li");
